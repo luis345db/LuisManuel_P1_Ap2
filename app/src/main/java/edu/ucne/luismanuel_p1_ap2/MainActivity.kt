9623aa48fd51878655ace5ec7cfb76b4791773d9
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-18 17:44:03 -0600

    Carga Inicial del Proyecto

---
+++ app/src/main/java/edu/ucne/luismanuel_p1_ap2/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.ucne.luismanuel_p1_ap2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.luismanuel_p1_ap2.ui.theme.LuisManuel_P1_Ap2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LuisManuel_P1_Ap2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LuisManuel_P1_Ap2Theme {
+        Greeting("Android")
+    }
+}
