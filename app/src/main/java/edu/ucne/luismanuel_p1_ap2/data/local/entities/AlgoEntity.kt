97b075868a2abb148d8d938e107871664192abff
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-18 19:06:50 -0600

    Agrega la Entidad

---
+++ app/src/main/java/edu/ucne/luismanuel_p1_ap2/data/local/entities/AlgoEntity.kt
@@ -0,0 +1,10 @@
+package edu.ucne.luismanuel_p1_ap2.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "algo")
+class AlgoEntity {
+    @PrimaryKey
+    val id : Int? = null
+}
