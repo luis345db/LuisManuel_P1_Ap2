26fc837111badd36e17d8b0e347c5d0a2f5a7d1e
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-18 18:06:17 -0600

    Agrega Hilt

---
+++ app/src/main/java/edu/ucne/luismanuel_p1_ap2/LuisManuel_P1_Ap2App.kt
@@ -0,0 +1,8 @@
+package edu.ucne.luismanuel_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LuisManuel_P1_Ap2App :Application() {
+}
--- app/src/main/java/edu/ucne/luismanuel_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/luismanuel_p1_ap2/MainActivity.kt
@@ -11,8 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.luismanuel_p1_ap2.ui.theme.LuisManuel_P1_Ap2Theme
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
