031bc783fbbe3f1762e83cdb1250186ca3485167
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-18 19:18:34 -0600

    Agrega el module

---
+++ app/src/main/java/edu/ucne/luismanuel_p1_ap2/dl/AppModule.kt
@@ -0,0 +1,30 @@
+package edu.ucne.luismanuel_p1_ap2.dl
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.luismanuel_p1_ap2.data.local.database.Parcial1Db
+import javax.inject.Singleton
+
+@InstallIn( SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideAlgoDb(@ApplicationContext appContext: Context)=
+        Room.databaseBuilder(
+            appContext,
+            Parcial1Db::class.java,
+            "Parcial1.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    fun provideAlgoDao(parcial1Db: Parcial1Db) = parcial1Db.algoDao()
+
+
+}
