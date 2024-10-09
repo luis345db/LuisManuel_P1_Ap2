924737c9b9438727123db273749c09e52bdb80e3
Luis Manuel <luis_duarte@ucne.edu.do>
2024-10-08 23:14:11 -0600

    agregue todo lo necesario para el swip to delete

--- app/src/main/java/edu/ucne/luismanuel_p1_ap2/presentation/venta/VentaListScreen.kt
+++ app/src/main/java/edu/ucne/luismanuel_p1_ap2/presentation/venta/VentaListScreen.kt
@@ -1,6 +1,14 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
 package edu.ucne.luismanuel_p1_ap2.presentation.venta
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,22 +18,35 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissState
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import edu.ucne.luismanuel_p1_ap2.data.local.entities.VentasEntity
 import edu.ucne.luismanuel_p1_ap2.presentation.venta.VentaViewModel.UiState
+import kotlinx.coroutines.delay
 
 @Composable
 fun VentaListScreen(
@@ -33,14 +54,16 @@ fun VentaListScreen(
 
     goToVenta: (Int) -> Unit,
     goToAddVenta: () -> Unit,
-    onEditVenta: () -> Unit,
-    onDeleteVenta: () -> Unit
+
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     ProductoListBodyScreen(
-        uiState,
-        goToVenta,
-        goToAddVenta
+        uiState = uiState,
+        goToVenta = goToVenta,
+        goToAddVenta = goToAddVenta,
+        onDeleteVenta = {venta ->
+            viewModel.delete(venta)
+        }
     )
 }
 
@@ -48,7 +71,8 @@ fun VentaListScreen(
 fun ProductoListBodyScreen(
     uiState: UiState,
     goToVenta: (Int) -> Unit,
-    goToAddVenta: () -> Unit
+    goToAddVenta: () -> Unit,
+    onDeleteVenta: (VentasEntity) -> Unit
 ) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -73,32 +97,37 @@ fun ProductoListBodyScreen(
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
             ) {
-                items(uiState.ventas) {
-                    ProductoRow(
-                        it,
-                        goToVenta,
-                        goToAddVenta
-                    )
+                items(uiState.ventas) {venta ->
+                    SwipeToDeleteContainer(
+                        item = venta,
+                        onDeleteVenta = onDeleteVenta
+                    ) { ventaItem, isSwipeable ->
+                        ProductoRow(
+                            it = ventaItem,
+                            goToVenta = goToVenta,
+                            isSwipeable = isSwipeable
+                        )
+                    }
                 }
             }
         }
     }
-
 }
+
 
 
 @Composable
 fun ProductoRow(
     it: VentasEntity,
     goToVenta: (Int) -> Unit,
-    goToAddVenta: () -> Unit
+    isSwipeable: Boolean = true
 ){
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 16.dp)
-            .clickable { goToVenta(it.ventaId ?: 0) }
+            .clickable(enabled = isSwipeable) { goToVenta(it.ventaId ?: 0) }
 
     ){
         Text(
@@ -132,5 +161,61 @@ fun ProductoRow(
 
 
 
+@Composable
+fun <T> SwipeToDeleteContainer(
+    item: T,
+    onDeleteVenta: (T) -> Unit,
+    animationDuration: Int = 500,
+    content: @Composable (T, Boolean) -> Unit
+) {
+    var isRemoved by remember { mutableStateOf(false) }
+    val state = rememberDismissState()
+
+    LaunchedEffect(key1 = state.currentValue) {
+        if (state.currentValue == DismissValue.DismissedToStart && !isRemoved) {
+            isRemoved = true
+            delay(animationDuration.toLong())
+            onDeleteVenta(item)
+        }
+    }
+
+    AnimatedVisibility(
+        visible = !isRemoved,
+        exit = shrinkVertically(
+            animationSpec = tween(durationMillis = animationDuration),
+            shrinkTowards = Alignment.Top
+        ) + fadeOut()
+    ) {
+        SwipeToDismiss(
+            state = state,
+            background = {
+                DeleteBackground(swipeDismissState = state)
+            },
+            dismissContent = { content(item, state.currentValue != DismissValue.DismissedToStart) },
+            directions = setOf(DismissDirection.EndToStart)
+        )
+    }
+}
+
+@Composable
+fun DeleteBackground(swipeDismissState: DismissState) {
+    val color = if (swipeDismissState.dismissDirection == DismissDirection.EndToStart) {
+        Color.Red
+    } else Color.Transparent
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color)
+            .padding(16.dp),
+        contentAlignment = Alignment.CenterEnd
+    ) {
+        Icon(
+            imageVector = Icons.Default.Delete,
+            contentDescription = null,
+            tint = Color.White
+        )
+    }
+}
 
 
